4f338b97f5374b23f44a616b5633bd905c048ec7
JerryYin <846597629@qq.com>
2015-10-27 17:43:18 +0800

    添加了Asynctask模拟网络加载，修改了文字为中文

--- app/src/main/java/com/example/jerryyin/mypulltorefresh/MainActivity.java
+++ app/src/main/java/com/example/jerryyin/mypulltorefresh/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.jerryyin.mypulltorefresh;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,7 +19,7 @@ public class MainActivity extends ActionBarActivity {
 
     private PullToRefreshListView mListView;
     private ArrayAdapter mAdapter;
-    private String[] arry = {"www", "ddddd", "222"};
+    private ArrayList<String> arry = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +27,38 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mListView = (PullToRefreshListView) findViewById(R.id.pull_to_refresh_list_view);
+        arry.add("1111111");
+        arry.add("22222222");
+        arry.add("ddddddd");
+
         mAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arry);
         mListView.setAdapter(mAdapter);
+        mListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+
+                new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        try {
+                            Thread.sleep(3000);
+
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+//                        super.onPostExecute(aVoid);
+                        arry.add("刷新出来的数据");
+                        arry.add("新数据2");
+                        mListView.onRefreshComplete();
+                    }
+                }.execute();
+            }
+        });
     }
 
 
